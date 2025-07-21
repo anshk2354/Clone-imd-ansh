681e9f5d8f45321edc721e5353ba3107df18f189
anshk2354 <155858616+anshk2354@users.noreply.github.com>
2025-07-21 09:00:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/mxmoder_video_download")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/864eb21067354a70fa921.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7732405661 -1083324269').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1083324269').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002655668331').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '-1083324269').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
